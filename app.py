9fa260c2c266ad2f00b239f0e97cef7fb0d24bc1
Madison Creese <mads@Madisons-MacBook-Pro.local>
2021-02-11 14:49:14 -0800

    fixed app

--- app.py
+++ app.py
@@ -6,21 +6,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///allaccess.db'
 CORS(app)
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-module.exports = allowCors(handler)
 
 db = SQLAlchemy(app)
 
